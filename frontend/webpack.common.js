87ff3363cdcfde0e2ff4cb190d5de3e092c09d7a
JusticeCodes00 <171641141+JusticeCodes00@users.noreply.github.com>
2025-11-23 02:38:23 +0100

    Prepare frontend for Vercel deployment

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -7,45 +7,47 @@ import { fileURLToPath } from "node:url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default {
-    entry: "./src/index.js",
-    output: {
-        filename: "[name].js",
-        path: resolve(__dirname, "../backend/src/dist"),
-        clean: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/template.html",
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/[name].css"
-        }),
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].js",
+    path: resolve(__dirname, "dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+    }),
+    new webpack.DefinePlugin({
+      "process.env.BACKEND_URL": JSON.stringify(process.env.BACKEND_URL),
+    }),
+  ],
+  optimization: {
+    minimizer: [
+      // For webpack v5, you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line // `...`,
+      new CssMinimizerPlugin(),
     ],
-    optimization: {
-        minimizer: [
-            // For webpack v5, you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line // `...`,
-            new CssMinimizerPlugin(),
-        ],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"]
-            },
-            {
-                test: /\.html$/i,
-                use: ["html-loader"]
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: "asset/resource",
-            },
-            {
-                test: /\.(woff|woff2)$/i,
-                type: 'asset/resource',
-            },
-
-        ]
-    }
-}
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
